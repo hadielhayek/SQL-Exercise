211d4f0f4a7fca0a28888a413c2dff53fb140af9
hadielhayek <hadi-hayek-20@hotmail.com>
2021-12-13 12:50:50 +0200

    basic queries

--- answers.sh
+++ answers.sh
@@ -1,1 +1,21 @@
-,,
+1.SELECT Name  FROM students
+
+2.SELECT * FROM students
+where age=30
+
+3.SELECT Name FROM students
+WHERE Age=30 AND Gender="F"
+
+4.SELECT Points FROM students
+where Name="Alex"
+
+5.INSERT INTO students
+(Name,Age,Gender,Points,ID) VALUES("Hadi",21,"M",400,7)
+
+6.UPDATE students
+SET Name='Basma', Points='350'
+WHERE ID=2;
+
+7.UPDATE students
+SET Name='Alex', Points='150'
+WHERE ID=1;
